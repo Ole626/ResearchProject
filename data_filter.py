bb26485c79f9c65211a75cb5f2216655b351168c
Ole Poeth <o.a.r.poeth@student.tudelft.nl>
2022-05-03 17:09:23 +0300

    Add attempt on removing outliers

--- data_filter.py
+++ data_filter.py
@@ -9,6 +9,7 @@ class DataFilter:
         self.saccades = None
         self.xy_coordinates = xy_coordinates
         self.threshold = 0.001
+        self.outlier_dist = 0.4
 
     def get_fixations(self):
         self.fixations = []
@@ -26,7 +27,7 @@ class DataFilter:
                 else:
                     current_fixation.append(self.xy_coordinates[i])
                     in_fixation = False
-                    self.fixations.append(self.average_point(current_fixation))
+                    self.fixations.append(self.mean_point(current_fixation))
                     current_fixation = []
 
         return self.fixations
@@ -35,7 +36,7 @@ class DataFilter:
         return math.sqrt(math.pow((coordinate1[0] - coordinate2[0]), 2)
                          + math.pow((coordinate1[1] - coordinate2[1]), 2))
 
-    def average_point(self, point_list):
+    def mean_point(self, point_list):
         x_cumulative = 0
         y_cumulative = 0
 
@@ -44,6 +45,16 @@ class DataFilter:
             y_cumulative = y_cumulative + point[1]
 
         return x_cumulative / len(point_list), y_cumulative / len(point_list)
+
+    def remove_outliers(self, xy_coordinates):
+        for i in range(0, math.floor(len(xy_coordinates)/500)):
+            mean_point = self.mean_point(xy_coordinates[i*500:(i+1)*500])
+            for point in xy_coordinates[i*500:(i+1)*500]:
+                dist = self.euclidean_distance(point, mean_point)
+                if dist > self.outlier_dist:
+                    xy_coordinates.remove(point)
+        return xy_coordinates
+
 
     def plot_fixations(self):
         rawdata = np.asarray(self.xy_coordinates)
--- main.py
+++ main.py
@@ -8,9 +8,10 @@ desktop_activity_location = ".\\data\\DesktopActivity\\subject5.mat"
 if __name__ == '__main__':
     fl = FileLoader()
     data = fl.read_file(desktop_activity_location)
-    fl.scatter_plot(data['readDataRaw'], "Read Raw")
     normalized = fl.normalize_data(data['readDataRaw'])
-    fl.scatter_plot(normalized, "Read Normalized")
+    df = DataFilter(normalized)
+    filtered = df.remove_outliers(normalized)
+    fl.scatter_plot(filtered, "Filtered")
 
 
 
