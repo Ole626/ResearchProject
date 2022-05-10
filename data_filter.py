e9e9bce5c374cb0df9008d93c3260174231924bf
Ole Poeth <o.a.r.poeth@student.tudelft.nl>
2022-05-10 12:35:17 +0200

    Add geometric median and part of fixation filter

--- data_filter.py
+++ data_filter.py
@@ -1,4 +1,6 @@
 import math
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -11,25 +13,54 @@ class DataFilter:
         self.threshold = 0.001
         self.outlier_dist = 0.4
 
-    def get_fixations(self):
+    def get_fixations(self, xy_coordinates, sliding_window, threshold, radius):
         self.fixations = []
-        current_fixation = []
-        in_fixation = False
+        difference_vector = np.zeros(len(xy_coordinates))
 
-        for i in range(0, len(self.xy_coordinates) - 2):
-            dist = self.euclidean_distance(self.xy_coordinates[i], self.xy_coordinates[i + 1])
-            if not in_fixation and dist <= self.threshold:
-                in_fixation = True
-                current_fixation.append(self.xy_coordinates[i])
-            if in_fixation:
-                if dist <= self.threshold:
-                    current_fixation.append(self.xy_coordinates[i])
-                else:
-                    current_fixation.append(self.xy_coordinates[i])
-                    in_fixation = False
-                    self.fixations.append(self.mean_point(current_fixation))
-                    current_fixation = []
+        # Get the difference vector for the mean of consecutive sliding windows
+        for i in range(sliding_window, len(xy_coordinates)-sliding_window-1):
+            m_before = [0, 0]
+            m_after = [0, 0]
 
+            for j in range(1, sliding_window):
+                m_before[0] = m_before[0] + xy_coordinates[i-j][0]/sliding_window
+                m_before[1] = m_before[1] + xy_coordinates[i-j][1]/sliding_window
+                m_after[0] = m_after[0] + xy_coordinates[i+j][0]/sliding_window
+                m_after[1] = m_after[1] + xy_coordinates[i+j][1]/sliding_window
+
+            difference_vector[i] = np.sqrt(math.pow((m_after[0] - m_before[0]), 2) +
+                                            math.pow((m_after[1] - m_before[1]), 2))
+
+        peaks = np.zeros(len(xy_coordinates))
+
+        # Get the peaks between averages and store them in a separate list
+        for i in range(1, len(xy_coordinates)-2):
+            if difference_vector[i] > difference_vector[i-1] and difference_vector[i] > difference_vector[i+1]:
+                peaks[i] = difference_vector[i]
+
+        # Remove peaks that are too close to each other
+        for i in range(sliding_window, len(xy_coordinates)-sliding_window-1):
+            if peaks[i] != 0:
+                for j in range(i-sliding_window, i-1):
+                    if peaks[j] <= peaks[i]:
+                        peaks[j] = 0
+                for j in range(i+1, i+sliding_window):
+                    if peaks[j] <= peaks[i]:
+                        peaks[j] = 0
+
+        peak_indices = []
+
+        for i in range(0, len(peaks)-1):
+            if peaks[i] >= threshold:
+                peak_indices.append(i)
+
+        shortest_distance = 0
+
+        #while shortest_distance < radius:
+        self.fixations = []
+        for i in range(1, len(peak_indices)-1):
+            self.fixations.append(self.geometric_median(xy_coordinates[peak_indices[i-1]:peak_indices[i]]))
+        print(self.fixations)
         return self.fixations
 
     def euclidean_distance(self, coordinate1, coordinate2):
@@ -53,14 +84,7 @@ class DataFilter:
             for j in range(0, sliding_window):
                 window.append(xy_coordinates[i+j-sliding_window])
 
-            avg_point = self.mean_point(window)
-            distances = []
-            for entry in window:
-                distances.append(self.euclidean_distance(entry, avg_point))
-
-            point_dist = list(zip(window, distances))
-            point_dist.sort(key=lambda tup: tup[1])
-            output = point_dist[int(np.floor(sliding_window/2))][0]
+            output = self.geometric_median(window)
 
             in_array = False
             for coordinate in output_coordinates:
@@ -72,6 +96,20 @@ class DataFilter:
 
         return output_coordinates
 
+    def geometric_median(self, xy_coordinates):
+        shortest_distance = sys.maxsize
+        median = []
+
+        for point in xy_coordinates:
+            distance_sum = 0
+            for comparison_point in xy_coordinates:
+                distance_sum = distance_sum + self.euclidean_distance(point, comparison_point)
+
+            if distance_sum < shortest_distance:
+                shortest_distance = distance_sum
+                median = point
+
+        return median
 
     def plot_fixations(self):
         rawdata = np.asarray(self.xy_coordinates)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from file_loader import FileLoader
 from data_filter import DataFilter
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 desktop_activity_location = ".\\data\\DesktopActivity\\subject7.mat"
@@ -9,11 +10,16 @@ if __name__ == '__main__':
     fl = FileLoader()
     data = fl.read_file(desktop_activity_location)
     name = 'searchDataRaw'
-    data_raw = data[name][0:10000]
+    data_raw = np.asarray(data[name][0:500])
     df = DataFilter(data_raw)
-    filtered = df.median_filter(data_raw, 200)
-    fl.position_over_time(data_raw)
-    fl.position_over_time(filtered)
+    median_point = df.geometric_median(data_raw)
+    filtered = np.asarray(df.median_filter(data_raw, 11))
+    fixations = np.asarray(df.get_fixations(filtered, 5, 0.01, 1))
+    plt.plot(filtered.T[0], filtered.T[1], color='blue')
+    plt.scatter(fixations.T[0], fixations.T[1], color='red')
+    plt.show()
+    #fl.position_over_time(data_raw)
+    #fl.position_over_time(filtered)
 
 
 
