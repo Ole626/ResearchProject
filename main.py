af4e365a68e430d3840051e43c237fabbd4f2f70
Ole Poeth <o.a.r.poeth@student.tudelft.nl>
2022-05-25 16:52:54 +0200

    Change hyperparameters

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from feature_extractor import FeatureExtractor
 import numpy as np
 
 
-SUBJECT = "subject3"
-NAME = 'readDataRaw'
+SUBJECT = "subject1"
+NAME = 'playDataRaw'
 LOCATION = ".\\data\\DesktopActivity\\" + SUBJECT + ".mat"
 DISTANCE_ARRAY = np.load('data\\' + SUBJECT + "\\" + NAME + '.npy')
 
@@ -13,18 +13,24 @@ DISTANCE_ARRAY = np.load('data\\' + SUBJECT + "\\" + NAME + '.npy')
 if __name__ == '__main__':
     fl = FileLoader()
     data = fl.read_file(LOCATION)
-    data_raw = fl.normalize_data(np.asarray(data[NAME])[0:5000])
+    data_raw = fl.normalize_data(np.asarray(data[NAME])[0:10000])
 
     df = DataFilter(data_raw, DISTANCE_ARRAY)
-    filtered = np.asarray(df.median_filter_with_distance_array(df.raw_data, 90))
-    fixations = np.asarray(df.get_fixations(df.filtered_data, 5, 0.001, 0.001))
+    filtered = np.asarray(df.median_filter_with_distance_array(df.raw_data, 6))
+    fixations = np.asarray(df.get_fixations(df.filtered_data, 8, 0.003, 0.001))
+    print(len(fixations))
+
+    # fl.position_over_time(data_raw)
+    # fl.position_over_time(filtered)
 
     fe = FeatureExtractor(filtered, df.peak_indices)
     fixation_durations = fe.fixation_duration(df.peak_indices)
-    saccade_lengths = fe.saccade_length(df.fixations)
+    # saccade_lengths = fe.saccade_length(df.fixations)
 
     fe.plot_features(fixation_durations, 'Fixation Duration ' + NAME + ' ' + SUBJECT, 'Fixation', 'Duration (ms)')
-    fe.plot_features(saccade_lengths, 'Saccade Length ' + NAME + ' ' + SUBJECT, 'Saccade', 'Length')
+    # fe.plot_features(saccade_lengths, 'Saccade Length ' + NAME + ' ' + SUBJECT, 'Saccade', 'Length')
+    print(fe.fixation_duration_avg_var_sd(df.peak_indices))
+
 
 
 
